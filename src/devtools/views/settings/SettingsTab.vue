991ab115d48f8fda10905ad9baa5e1187c4824df
skeptycal <skeptycal@gmail.com>
2019-06-24 12:45:30 -0500

    formatting

--- src/devtools/views/settings/SettingsTab.vue
+++ src/devtools/views/settings/SettingsTab.vue
@@ -5,42 +5,47 @@
 </template>
 
 <script>
-import GlobalPreferences from './GlobalPreferences.vue'
-import { mapState } from 'vuex'
+import GlobalPreferences from "./GlobalPreferences.vue";
+import { mapState } from "vuex";
 
-export const SETTINGS_VERSION = 1
-export const SETTINGS_VERSION_ID = 'vue-devtools-settings-version'
+export const SETTINGS_VERSION = 1;
+export const SETTINGS_VERSION_ID = "vue-devtools-settings-version";
 
 export default {
   components: { GlobalPreferences },
 
-  provide () {
+  provide() {
     return {
       settingsVersion: SETTINGS_VERSION,
-      currentSettingsVersion: parseInt(localStorage.getItem(SETTINGS_VERSION_ID)) || 0
-    }
+      currentSettingsVersion:
+        parseInt(localStorage.getItem(SETTINGS_VERSION_ID)) || 0
+    };
   },
 
-  computed: mapState('events', [
-    'enabled'
-  ])
-}
+  computed: mapState("events", ["enabled"])
+};
 </script>
 
 <style lang="stylus" scoped>
-.settings
-  overflow auto !important
-  >>> .preferences
-    display flex
-    flex-wrap wrap
-    padding 12px 4px
+.settings {
+  overflow: auto !important;
 
-    > *
-      flex-basis 300px
-      margin 16px 24px
+  >>> .preferences {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 12px 4px;
 
-    .vue-ui-form-field
-      > .wrapper > .content
-       min-height 32px
-       justify-content center
+    > * {
+      flex-basis: 300px;
+      margin: 16px 24px;
+    }
+
+    .vue-ui-form-field {
+      > .wrapper > .content {
+        min-height: 32px;
+        justify-content: center;
+      }
+    }
+  }
+}
 </style>
