4b7fa5c7d2864389f1f32308093b6b02cccb940b
skeptycal <skeptycal@gmail.com>
2019-06-24 12:43:37 -0500

    formatting

--- shells/dev/target/EventChild.vue
+++ shells/dev/target/EventChild.vue
@@ -4,7 +4,7 @@
     <button class="btn-emit-event1" @click="emitEvent1">Emit</button>
     <button class="btn-emit-event2" @click="emitEvent2">Emit</button>
 
-    <br>
+    <br />
 
     <button @click="emitManyEvents">Emit a lot of events</button>
     <button @click="emitAndCommit">Emit and event and commit a mutation</button>
@@ -14,44 +14,44 @@
 <script>
 export default {
   methods: {
-    emitEvent () {
+    emitEvent() {
       const data = {
-        eventName: 'event'
-      }
-      this.$emit('event', data)
+        eventName: "event"
+      };
+      this.$emit("event", data);
     },
-    emitEvent1 () {
+    emitEvent1() {
       const data = {
-        eventName: 'event-1'
-      }
-      this.$emit('event-1', data)
+        eventName: "event-1"
+      };
+      this.$emit("event-1", data);
     },
-    emitEvent2 () {
+    emitEvent2() {
       const complexData = {
-        componentName: 'EventChild',
-        string: 'Lorem ipsum',
+        componentName: "EventChild",
+        string: "Lorem ipsum",
         complex: {
-          string: 'Lorem ipsum',
+          string: "Lorem ipsum",
           object: {
             number: 23,
             boolean: true,
             array: [1, 2, 3, 4, 5]
           }
         }
-      }
-      this.$emit('event-2', complexData)
+      };
+      this.$emit("event-2", complexData);
     },
 
-    emitManyEvents () {
+    emitManyEvents() {
       for (let i = 0; i < 10000; i++) {
-        this.$emit('event', i)
+        this.$emit("event", i);
       }
     },
 
-    emitAndCommit () {
-      this.$emit('event-1', 'foobar')
-      this.$store.commit('DECREMENT', 'barfoo')
+    emitAndCommit() {
+      this.$emit("event-1", "foobar");
+      this.$store.commit("DECREMENT", "barfoo");
     }
   }
-}
+};
 </script>
