a62dc9d438a441095ddf6377e9879dd6db4808e7
skeptycal <skeptycal@gmail.com>
2019-06-24 12:44:51 -0500

    formatting

--- src/devtools/mixins/defer.js
+++ src/devtools/mixins/defer.js
@@ -1,32 +1,32 @@
-export default function (count = 10) {
+export default function(count = 10) {
   // @vue/component
   return {
-    data () {
+    data() {
       return {
         displayPriority: 0
-      }
+      };
     },
 
-    mounted () {
-      this.runDisplayPriority()
+    mounted() {
+      this.runDisplayPriority();
     },
 
     methods: {
-      runDisplayPriority () {
+      runDisplayPriority() {
         const step = () => {
           requestAnimationFrame(() => {
-            this.displayPriority++
+            this.displayPriority++;
             if (this.displayPriority < count) {
-              step()
+              step();
             }
-          })
-        }
-        step()
+          });
+        };
+        step();
       },
 
-      defer (priority) {
-        return this.displayPriority >= priority
+      defer(priority) {
+        return this.displayPriority >= priority;
       }
     }
-  }
+  };
 }
