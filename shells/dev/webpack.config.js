83f42cb6515f139d36633ee2f3897f47e519237a
skeptycal <skeptycal@gmail.com>
2019-06-24 12:44:05 -0500

    formatting

--- shells/dev/webpack.config.js
+++ shells/dev/webpack.config.js
@@ -1,23 +1,23 @@
-const createConfig = require('../createConfig')
-const openInEditor = require('launch-editor-middleware')
+const createConfig = require("../createConfig");
+const openInEditor = require("launch-editor-middleware");
 
 module.exports = createConfig({
   entry: {
-    devtools: './src/devtools.js',
-    backend: './src/backend.js',
-    hook: './src/hook.js',
-    target: './target/index.js'
+    devtools: "./src/devtools.js",
+    backend: "./src/backend.js",
+    hook: "./src/hook.js",
+    target: "./target/index.js"
   },
   output: {
-    path: __dirname + '/build',
-    publicPath: '/build/',
-    filename: '[name].js'
+    path: __dirname + "/build",
+    publicPath: "/build/",
+    filename: "[name].js"
   },
-  devtool: '#cheap-module-source-map',
+  devtool: "#cheap-module-source-map",
   devServer: {
     quiet: true,
-    before (app) {
-      app.use('/__open-in-editor', openInEditor())
+    before(app) {
+      app.use("/__open-in-editor", openInEditor());
     }
   }
-})
+});
