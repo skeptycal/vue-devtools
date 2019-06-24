2c67c736cad09037bd2620e147816c53600cb12d
skeptycal <skeptycal@gmail.com>
2019-06-24 12:44:16 -0500

    formatting

--- shells/electron/src/hook.js
+++ shells/electron/src/hook.js
@@ -1,4 +1,4 @@
-import { installHook } from 'src/backend/hook'
-import { target } from 'src/devtools/env'
+import { installHook } from "src/backend/hook";
+import { target } from "src/devtools/env";
 
-installHook(target)
+installHook(target);
