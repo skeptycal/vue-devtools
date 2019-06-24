0a031bee12456e9f290880071a216f0b202d336a
skeptycal <skeptycal@gmail.com>
2019-06-24 12:42:37 -0500

    formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,23 @@
 module.exports = {
-  'root': true,
-  'env': {
-    'browser': true
+  root: true,
+  env: {
+    browser: true
   },
-  'extends': [
-    'standard',
-    'plugin:vue/recommended'
-  ],
-  'globals': {
-    'bridge': true,
-    'chrome': true,
-    'localStorage': true,
-    'HTMLDocument': true,
-    'name': 'off'
+  extends: ["standard", "plugin:vue/recommended"],
+  globals: {
+    bridge: true,
+    chrome: true,
+    localStorage: true,
+    HTMLDocument: true,
+    name: "off"
   },
-  'rules': {
-    'vue/html-closing-bracket-newline': ['error', {
-      'singleline': 'never',
-      'multiline': 'always'
-    }]
+  rules: {
+    "vue/html-closing-bracket-newline": [
+      "error",
+      {
+        singleline: "never",
+        multiline: "always"
+      }
+    ]
   }
-}
+};
