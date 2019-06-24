58916f28719396e6eaf35e9f95a0b095af934049
skeptycal <skeptycal@gmail.com>
2019-06-24 12:45:13 -0500

    formatting

--- src/devtools/views/perf/ComponentRenderDetails.vue
+++ src/devtools/views/perf/ComponentRenderDetails.vue
@@ -1,9 +1,6 @@
 <template>
   <ScrollPane class="component-render-details">
-    <div
-      v-if="!entry"
-      class="vue-ui-empty"
-    >
+    <div v-if="!entry" class="vue-ui-empty">
       No component selected
     </div>
 
@@ -24,19 +21,11 @@
         }"
       >
         <div class="header">
-          <div
-            v-for="column of columns"
-            :key="column"
-            class="column"
-          >
+          <div v-for="column of columns" :key="column" class="column">
             {{ column }}
           </div>
         </div>
-        <div
-          v-for="e of entries"
-          :key="e.id"
-          class="metric selectable-item"
-        >
+        <div v-for="e of entries" :key="e.id" class="metric selectable-item">
           <div
             class="type"
             :class="{
@@ -79,30 +68,25 @@
 </template>
 
 <script>
-import { classify } from 'src/util'
+import { classify } from "src/util";
 
-import ScrollPane from 'components/ScrollPane.vue'
-import ActionHeader from 'components/ActionHeader.vue'
+import ScrollPane from "components/ScrollPane.vue";
+import ActionHeader from "components/ActionHeader.vue";
 
 const ENTRIES = [
-  'beforeCreate',
-  'created',
-  'beforeMount',
-  'mountRender',
-  'mounted',
-  'beforeUpdate',
-  'updateRender',
-  'updated',
-  'beforeDestroyed',
-  'destroyed'
-]
+  "beforeCreate",
+  "created",
+  "beforeMount",
+  "mountRender",
+  "mounted",
+  "beforeUpdate",
+  "updateRender",
+  "updated",
+  "beforeDestroyed",
+  "destroyed"
+];
 
-const COLUMNS = [
-  'type',
-  'count',
-  'total time',
-  'average time'
-]
+const COLUMNS = ["type", "count", "total time", "average time"];
 
 export default {
   components: {
@@ -118,62 +102,80 @@ export default {
   },
 
   computed: {
-    entries () {
+    entries() {
       return ENTRIES.map(type => ({
         id: type,
-        ...this.entry.hooks[type] || { totalTime: 0, count: 0 }
-      }))
+        ...(this.entry.hooks[type] || { totalTime: 0, count: 0 })
+      }));
     },
 
-    componentName () {
-      return (this.$shared.classifyComponents ? classify(this.entry.id) : this.entry.id) || 'Anonymous Component'
+    componentName() {
+      return (
+        (this.$shared.classifyComponents
+          ? classify(this.entry.id)
+          : this.entry.id) || "Anonymous Component"
+      );
     },
 
-    highDensity () {
-      const pref = this.$shared.displayDensity
-      return (pref === 'auto' && this.entries.length > 8) || pref === 'high'
+    highDensity() {
+      const pref = this.$shared.displayDensity;
+      return (pref === "auto" && this.entries.length > 8) || pref === "high";
     }
   },
 
-  created () {
-    this.columns = COLUMNS
+  created() {
+    this.columns = COLUMNS;
   }
-}
+};
 </script>
 
 <style lang="stylus" scoped>
-.title
-  white-space nowrap
-  position relative
-  top -1px
+.title {
+  white-space: nowrap;
+  position: relative;
+  top: -1px;
+}
 
-.metrics
-  padding 6px 0
-  font-size 14px
-  &.high-density
-    font-size 12px
+.metrics {
+  padding: 6px 0;
+  font-size: 14px;
 
-.header,
-.metric
-  display flex
-  /deep/ > *
-    flex 25% 0 0
-    padding 4px 10px
-    .high-density &
-      padding 2px 10px
-    &:not(:first-child)
-      text-align right
+  &.high-density {
+    font-size: 12px;
+  }
+}
 
-  .dim
-    opacity .4
+.header, .metric {
+  display: flex;
 
-.header
-  color $blueishGrey
-  margin-bottom 6px
+  /deep/ > * {
+    flex: 25% 0 0;
+    padding: 4px 10px;
 
-.metric
-  font-family Menlo, Consolas, monospace
+    .high-density & {
+      padding: 2px 10px;
+    }
 
-.type
-  color $green
+    &:not(:first-child) {
+      text-align: right;
+    }
+  }
+
+  .dim {
+    opacity: 0.4;
+  }
+}
+
+.header {
+  color: $blueishGrey;
+  margin-bottom: 6px;
+}
+
+.metric {
+  font-family: Menlo, Consolas, monospace;
+}
+
+.type {
+  color: $green;
+}
 </style>
