3b2da8049a5f8cc152e61066cd9af3e334c1f057
skeptycal <skeptycal@gmail.com>
2019-06-24 12:44:10 -0500

    formatting

--- shells/electron/bin.js
+++ shells/electron/bin.js
@@ -1,12 +1,10 @@
 #!/usr/bin/env node
-const electron = require('electron')
-const spawn = require('cross-spawn')
-const argv = process.argv.slice(2)
+const electron = require("electron");
+const spawn = require("cross-spawn");
+const argv = process.argv.slice(2);
 
-const result = spawn.sync(
-  electron,
-  [require.resolve('./app')].concat(argv),
-  { stdio: 'ignore' }
-)
+const result = spawn.sync(electron, [require.resolve("./app")].concat(argv), {
+  stdio: "ignore"
+});
 
-process.exit(result.status)
+process.exit(result.status);
