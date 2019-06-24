f66a5ca660f44643b2dc1d6f961bca838014b174
skeptycal <skeptycal@gmail.com>
2019-06-24 12:43:23 -0500

    formatting

--- shells/chrome/src/detector.js
+++ shells/chrome/src/detector.js
@@ -1,71 +1,77 @@
-import { installToast } from 'src/backend/toast'
-import { isFirefox } from 'src/devtools/env'
+import { installToast } from "src/backend/toast";
+import { isFirefox } from "src/devtools/env";
 
-window.addEventListener('message', e => {
+window.addEventListener("message", e => {
   if (e.source === window && e.data.vueDetected) {
-    chrome.runtime.sendMessage(e.data)
+    chrome.runtime.sendMessage(e.data);
   }
-})
+});
 
-function detect (win) {
+function detect(win) {
   setTimeout(() => {
     // Method 1: Check Nuxt.js
-    const nuxtDetected = Boolean(window.__NUXT__ || window.$nuxt)
+    const nuxtDetected = Boolean(window.__NUXT__ || window.$nuxt);
 
     if (nuxtDetected) {
-      let Vue
+      let Vue;
 
       if (window.$nuxt) {
-        Vue = window.$nuxt.$root.constructor
+        Vue = window.$nuxt.$root.constructor;
       }
 
-      win.postMessage({
-        devtoolsEnabled: Vue && Vue.config.devtools,
-        vueDetected: true,
-        nuxtDetected: true
-      }, '*')
+      win.postMessage(
+        {
+          devtoolsEnabled: Vue && Vue.config.devtools,
+          vueDetected: true,
+          nuxtDetected: true
+        },
+        "*"
+      );
 
-      return
+      return;
     }
 
     // Method 2: Scan all elements inside document
-    const all = document.querySelectorAll('*')
-    let el
+    const all = document.querySelectorAll("*");
+    let el;
     for (let i = 0; i < all.length; i++) {
       if (all[i].__vue__) {
-        el = all[i]
-        break
+        el = all[i];
+        break;
       }
     }
     if (el) {
-      let Vue = Object.getPrototypeOf(el.__vue__).constructor
+      let Vue = Object.getPrototypeOf(el.__vue__).constructor;
       while (Vue.super) {
-        Vue = Vue.super
+        Vue = Vue.super;
       }
-      win.postMessage({
-        devtoolsEnabled: Vue.config.devtools,
-        vueDetected: true
-      }, '*')
+      win.postMessage(
+        {
+          devtoolsEnabled: Vue.config.devtools,
+          vueDetected: true
+        },
+        "*"
+      );
     }
-  }, 100)
+  }, 100);
 }
 
 // inject the hook
 if (document instanceof HTMLDocument) {
-  installScript(detect)
-  installScript(installToast)
+  installScript(detect);
+  installScript(installToast);
 }
 
-function installScript (fn) {
-  const source = ';(' + fn.toString() + ')(window)'
+function installScript(fn) {
+  const source = ";(" + fn.toString() + ")(window)";
 
   if (isFirefox) {
     // eslint-disable-next-line no-eval
-    window.eval(source) // in Firefox, this evaluates on the content window
+    window.eval(source); // in Firefox, this evaluates on the content window
   } else {
-    const script = document.createElement('script')
-    script.textContent = source
-    document.documentElement.appendChild(script)
-    script.parentNode.removeChild(script)
+    const script = document.createElement("script");
+    script.textContent = source;
+    document.documentElement.appendChild(script);
+    script.parentNode.removeChild(script);
   }
 }
