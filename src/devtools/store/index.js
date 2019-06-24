e57dc5372a79c46917b88584a404a985448344cf
skeptycal <skeptycal@gmail.com>
2019-06-24 12:44:59 -0500

    formatting

--- src/devtools/store/index.js
+++ src/devtools/store/index.js
@@ -1,28 +1,28 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import components from 'views/components/module'
-import vuex from 'views/vuex/module'
-import events from 'views/events/module'
-import router from 'views/router/module'
-import routes from 'views/routes/module'
-import perf from 'views/perf/module'
+import Vue from "vue";
+import Vuex from "vuex";
+import components from "views/components/module";
+import vuex from "views/vuex/module";
+import events from "views/events/module";
+import router from "views/router/module";
+import routes from "views/routes/module";
+import perf from "views/perf/module";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
-    message: '',
-    view: 'vertical'
+    message: "",
+    view: "vertical"
   },
   mutations: {
-    SHOW_MESSAGE (state, message) {
-      state.message = message
+    SHOW_MESSAGE(state, message) {
+      state.message = message;
     },
-    SWITCH_VIEW (state, view) {
-      state.view = view
+    SWITCH_VIEW(state, view) {
+      state.view = view;
     },
-    RECEIVE_INSTANCE_DETAILS (state, instance) {
-      state.message = 'Instance selected: ' + instance.name
+    RECEIVE_INSTANCE_DETAILS(state, instance) {
+      state.message = "Instance selected: " + instance.name;
     }
   },
   modules: {
@@ -33,32 +33,35 @@ const store = new Vuex.Store({
     routes,
     perf
   }
-})
+});
 
-export default store
+export default store;
 
 if (module.hot) {
-  module.hot.accept([
-    'views/components/module',
-    'views/vuex/module',
-    'views/events/module',
-    'views/router/module',
-    'views/routes/module',
-    'views/perf/module'
-  ], () => {
-    try {
-      store.hotUpdate({
-        modules: {
-          components: require('views/components/module').default,
-          vuex: require('views/vuex/module').default,
-          events: require('views/events/module').default,
-          router: require('views/router/module').default,
-          routes: require('views/routes/module').default,
-          perf: require('views/perf/module').default
-        }
-      })
-    } catch (e) {
-      console.log(e.stack)
+  module.hot.accept(
+    [
+      "views/components/module",
+      "views/vuex/module",
+      "views/events/module",
+      "views/router/module",
+      "views/routes/module",
+      "views/perf/module"
+    ],
+    () => {
+      try {
+        store.hotUpdate({
+          modules: {
+            components: require("views/components/module").default,
+            vuex: require("views/vuex/module").default,
+            events: require("views/events/module").default,
+            router: require("views/router/module").default,
+            routes: require("views/routes/module").default,
+            perf: require("views/perf/module").default
+          }
+        });
+      } catch (e) {
+        console.log(e.stack);
+      }
     }
-  })
+  );
 }
