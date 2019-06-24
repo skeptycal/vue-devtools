5061078f134a43b522acb62128b92517700c9c71
skeptycal <skeptycal@gmail.com>
2019-06-24 12:45:09 -0500

    formatting

--- src/devtools/views/events/EventInspector.vue
+++ src/devtools/views/events/EventInspector.vue
@@ -1,26 +1,19 @@
 <template>
   <scroll-pane>
-    <div
-      v-if="activeEvent"
-      slot="scroll"
-    >
+    <div v-if="activeEvent" slot="scroll">
       <state-inspector :state="{ 'event info': sortedEventData }" />
     </div>
-    <div
-      v-else
-      slot="scroll"
-      class="no-event-data"
-    >
+    <div v-else slot="scroll" class="no-event-data">
       No event selected
     </div>
   </scroll-pane>
 </template>
 
 <script>
-import ScrollPane from 'components/ScrollPane.vue'
-import StateInspector from 'components/StateInspector.vue'
+import ScrollPane from "components/ScrollPane.vue";
+import StateInspector from "components/StateInspector.vue";
 
-import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
 
 export default {
   components: {
@@ -29,43 +22,48 @@ export default {
   },
 
   computed: {
-    ...mapGetters('events', [
-      'activeEvent'
-    ]),
+    ...mapGetters("events", ["activeEvent"]),
 
-    sortedEventData () {
+    sortedEventData() {
       if (!this.activeEvent) {
-        return {}
+        return {};
       }
       return {
         name: this.activeEvent.eventName,
         type: this.activeEvent.type,
-        source: '<' + this.activeEvent.instanceName + '>',
+        source: "<" + this.activeEvent.instanceName + ">",
         payload: this.activeEvent.payload
-      }
+      };
     }
   }
-}
+};
 </script>
 
 <style lang="stylus" scoped>
-section:not(:last-child)
-  border-bottom 1px solid $border-color
-  .vue-ui-dark-mode &
-    border-bottom 1px solid $dark-border-color
+section:not(:last-child) {
+  border-bottom: 1px solid $border-color;
 
-.component-name
-  margin 0 10px
+  .vue-ui-dark-mode & {
+    border-bottom: 1px solid $dark-border-color;
+  }
+}
 
-.string
-  color: $red
+.component-name {
+  margin: 0 10px;
+}
 
-.literal
-  color: #03c
+.string {
+  color: $red;
+}
 
-.no-event-data
+.literal {
+  color: #03c;
+}
+
+.no-event-data {
   color: #ccc;
   text-align: center;
   margin-top: 50px;
   line-height: 30px;
+}
 </style>
