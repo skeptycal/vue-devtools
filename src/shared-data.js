df5d522a4183742900c9d55b0c38fbdf2e5c4c59
skeptycal <skeptycal@gmail.com>
2019-06-24 12:45:38 -0500

    formatting

--- src/shared-data.js
+++ src/shared-data.js
@@ -1,11 +1,11 @@
-import storage from './storage'
+import storage from "./storage";
 
 // Initial state
 const internalSharedData = {
-  openInEditorHost: '/',
+  openInEditorHost: "/",
   classifyComponents: true,
-  theme: 'auto',
-  displayDensity: 'low',
+  theme: "auto",
+  displayDensity: "low",
   recordVuex: true,
   cacheVuexSnapshotsEvery: 50,
   cacheVuexSnapshotsLimit: 10,
@@ -15,94 +15,95 @@ const internalSharedData = {
   logDetected: true,
   vuexNewBackend: false,
   vuexAutoload: false
-}
+};
 
 const persisted = [
-  'classifyComponents',
-  'theme',
-  'displayDensity',
-  'recordVuex',
-  'editableProps',
-  'logDetected',
-  'vuexNewBackend',
-  'vuexAutoload'
-]
+  "classifyComponents",
+  "theme",
+  "displayDensity",
+  "recordVuex",
+  "editableProps",
+  "logDetected",
+  "vuexNewBackend",
+  "vuexAutoload"
+];
 
 // ---- INTERNALS ---- //
 
-let Vue
-let bridge
+let Vue;
+let bridge;
 // List of fields to persist to storage (disabled if 'false')
 // This should be unique to each shared data client to prevent conflicts
-let persist = false
+let persist = false;
 // For reactivity, we wrap the data in a Vue instance
-let vm
+let vm;
 
-export function init (params) {
+export function init(params) {
   // Mandatory params
-  bridge = params.bridge
-  Vue = params.Vue
-  persist = !!params.persist
+  bridge = params.bridge;
+  Vue = params.Vue;
+  persist = !!params.persist;
 
   // Load persisted fields
   persisted.forEach(key => {
-    const value = storage.get(`shared-data:${key}`)
+    const value = storage.get(`shared-data:${key}`);
     if (value !== null) {
-      internalSharedData[key] = value
+      internalSharedData[key] = value;
       // Send to other shared data clients
       if (persist) {
-        sendValue(key, value)
+        sendValue(key, value);
       }
     }
-  })
+  });
 
   // Wrapper Vue instance
   vm = new Vue({
     data: internalSharedData
-  })
+  });
 
   // Update value from other shared data clients
-  bridge.on('shared-data:set', ({ key, value }) => {
-    setValue(key, value)
-  })
+  bridge.on("shared-data:set", ({ key, value }) => {
+    setValue(key, value);
+  });
 }
 
-export function destroy () {
-  bridge.removeAllListeners('shared-data:set')
-  vm.$destroy()
+export function destroy() {
+  bridge.removeAllListeners("shared-data:set");
+  vm.$destroy();
 }
 
-function setValue (key, value) {
+function setValue(key, value) {
   // Storage
   if (persist && persisted.includes(key)) {
-    storage.set(`shared-data:${key}`, value)
+    storage.set(`shared-data:${key}`, value);
   }
-  vm[key] = value
+  vm[key] = value;
   // Validate Proxy set trap
-  return true
+  return true;
 }
 
-function sendValue (key, value) {
-  bridge && bridge.send('shared-data:set', {
-    key,
-    value
-  })
+function sendValue(key, value) {
+  bridge &&
+    bridge.send("shared-data:set", {
+      key,
+      value
+    });
 }
 
-export function watch (...args) {
-  vm.$watch(...args)
+export function watch(...args) {
+  vm.$watch(...args);
 }
 
-const proxy = {}
+const proxy = {};
 Object.keys(internalSharedData).forEach(key => {
   Object.defineProperty(proxy, key, {
     configurable: false,
     get: () => vm && vm.$data[key],
-    set: (value) => {
-      sendValue(key, value)
-      setValue(key, value)
+    set: value => {
+      sendValue(key, value);
+      setValue(key, value);
     }
-  })
-})
+  });
+});
 
-export default proxy
+export default proxy;
