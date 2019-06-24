f39773910c6a1fe0e0fd1a70af8bd625bd233611
skeptycal <skeptycal@gmail.com>
2019-06-24 12:45:24 -0500

    formatting

--- src/devtools/views/routes/RoutesTab.vue
+++ src/devtools/views/routes/RoutesTab.vue
@@ -4,10 +4,7 @@
       <routes-tree slot="left" />
       <routes-meta slot="right" />
     </split-pane>
-    <div
-      v-else
-      class="notice"
-    >
+    <div v-else class="notice">
       <div>
         No routes detected.
       </div>
@@ -16,10 +13,10 @@
 </template>
 
 <script>
-import SplitPane from 'components/SplitPane.vue'
-import RoutesTree from './RoutesTree.vue'
-import RoutesMeta from './RoutesMeta.vue'
-import { mapState } from 'vuex'
+import SplitPane from "components/SplitPane.vue";
+import RoutesTree from "./RoutesTree.vue";
+import RoutesMeta from "./RoutesMeta.vue";
+import { mapState } from "vuex";
 
 export default {
   components: {
@@ -28,8 +25,8 @@ export default {
     RoutesTree
   },
 
-  computed: mapState('routes', {
+  computed: mapState("routes", {
     hasRouter: state => state.hasRouter
   })
-}
+};
 </script>
