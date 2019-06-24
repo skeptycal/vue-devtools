aae16abb56217552ddf05327f8607148aa1b3b73
skeptycal <skeptycal@gmail.com>
2019-06-24 12:43:46 -0500

    formatting

--- shells/dev/target/RefTester.vue
+++ shells/dev/target/RefTester.vue
@@ -3,11 +3,7 @@
     <p ref="tester" id="testing" class="test test1">{{ count }}</p>
 
     <ul>
-      <li
-        v-for="i in 4"
-        :key="i"
-        ref="list"
-      >{{ i }}</li>
+      <li v-for="i in 4" :key="i" ref="list">{{ i }}</li>
     </ul>
   </div>
 </template>
@@ -15,7 +11,9 @@
 <script>
 export default {
   computed: {
-    count() { return 1 },
+    count() {
+      return 1;
+    }
   }
-}
+};
 </script>
