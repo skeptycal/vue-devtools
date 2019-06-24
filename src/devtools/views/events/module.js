6f40cb530db58419ee03cc229c92c559dfeb1eb5
skeptycal <skeptycal@gmail.com>
2019-06-24 12:45:12 -0500

    formatting

--- src/devtools/views/events/module.js
+++ src/devtools/views/events/module.js
@@ -1,96 +1,99 @@
-import storage from 'src/storage'
-import { classify } from 'src/util'
-import SharedData from 'src/shared-data'
+import storage from "src/storage";
+import { classify } from "src/util";
+import SharedData from "src/shared-data";
 
-const ENABLED_KEY = 'EVENTS_ENABLED'
-const enabled = storage.get(ENABLED_KEY)
-const REGEX_RE = /^\/((?:(?:.*?)(?:\\\/)?)*?)\/(\w*)/
+const ENABLED_KEY = "EVENTS_ENABLED";
+const enabled = storage.get(ENABLED_KEY);
+const REGEX_RE = /^\/((?:(?:.*?)(?:\\\/)?)*?)\/(\w*)/;
 
-let uid = 0
+let uid = 0;
 
 const state = {
   enabled: enabled == null ? true : enabled,
   events: [],
   inspectedIndex: -1,
   newEventCount: 0,
-  filter: ''
-}
+  filter: ""
+};
 
 const mutations = {
-  'RECEIVE_EVENT' (state, payload) {
-    payload.id = uid++
-    state.events.push(payload)
+  RECEIVE_EVENT(state, payload) {
+    payload.id = uid++;
+    state.events.push(payload);
     if (!state.filter) {
-      state.inspectedIndex = state.events.length - 1
+      state.inspectedIndex = state.events.length - 1;
     }
   },
-  'RESET' (state) {
-    state.events = []
-    state.inspectedIndex = -1
+  RESET(state) {
+    state.events = [];
+    state.inspectedIndex = -1;
   },
-  'INSPECT' (state, index) {
-    state.inspectedIndex = index
+  INSPECT(state, index) {
+    state.inspectedIndex = index;
   },
-  'RESET_NEW_EVENT_COUNT' (state) {
-    state.newEventCount = 0
+  RESET_NEW_EVENT_COUNT(state) {
+    state.newEventCount = 0;
   },
-  'INCREASE_NEW_EVENT_COUNT' (state) {
-    state.newEventCount++
+  INCREASE_NEW_EVENT_COUNT(state) {
+    state.newEventCount++;
   },
-  'UPDATE_FILTER' (state, filter) {
-    state.filter = filter
+  UPDATE_FILTER(state, filter) {
+    state.filter = filter;
   },
-  'TOGGLE' (state) {
-    storage.set(ENABLED_KEY, state.enabled = !state.enabled)
-    bridge.send('events:toggle-recording', state.enabled)
+  TOGGLE(state) {
+    storage.set(ENABLED_KEY, (state.enabled = !state.enabled));
+    bridge.send("events:toggle-recording", state.enabled);
   }
-}
+};
 
 const matchingEvent = ({ searchText, searchComponent, regEx }) => e => {
-  const classifyComponents = SharedData.classifyComponents
-  let searchTerm = (searchComponent
-    ? (classifyComponents
-      ? classify(e.instanceName) : e.instanceName)
-    : e.eventName)
+  const classifyComponents = SharedData.classifyComponents;
+  let searchTerm = searchComponent
+    ? classifyComponents
+      ? classify(e.instanceName)
+      : e.instanceName
+    : e.eventName;
 
   if (regEx) {
     try {
-      return regEx.test(searchTerm)
+      return regEx.test(searchTerm);
     } catch (e) {
-      return searchTerm.toLowerCase().indexOf(searchText) > -1
+      return searchTerm.toLowerCase().indexOf(searchText) > -1;
     }
   }
 
-  return searchTerm.toLowerCase().indexOf(searchText) > -1
-}
+  return searchTerm.toLowerCase().indexOf(searchText) > -1;
+};
 
 const getters = {
   activeEvent: (state, getters) => {
-    return getters.filteredEvents[state.inspectedIndex]
+    return getters.filteredEvents[state.inspectedIndex];
   },
   filteredEvents: (state, getters, rootState) => {
-    let searchText = state.filter.toLowerCase()
-    const searchComponent = /<|>/g.test(searchText)
+    let searchText = state.filter.toLowerCase();
+    const searchComponent = /<|>/g.test(searchText);
     if (searchComponent) {
-      searchText = searchText.replace(/<|>/g, '')
+      searchText = searchText.replace(/<|>/g, "");
     }
-    const regExParts = state.filter.match(REGEX_RE)
-    let regEx
+    const regExParts = state.filter.match(REGEX_RE);
+    let regEx;
     if (regExParts) {
-      regEx = new RegExp(regExParts[1], regExParts[2])
+      regEx = new RegExp(regExParts[1], regExParts[2]);
     }
-    return state.events
-      .filter(matchingEvent({ searchText, searchComponent, regEx }))
+    return state.events.filter(
+      matchingEvent({ searchText, searchComponent, regEx })
+    );
   }
-}
+};
 
 const actions = {
   inspect: ({ commit, getters }, index) => {
-    if (index < 0) index = 0
-    if (index >= getters.filteredEvents.length) index = getters.filteredEvents.length - 1
-    commit('INSPECT', index)
+    if (index < 0) index = 0;
+    if (index >= getters.filteredEvents.length)
+      index = getters.filteredEvents.length - 1;
+    commit("INSPECT", index);
   }
-}
+};
 
 export default {
   namespaced: true,
@@ -98,4 +101,4 @@ export default {
   mutations,
   getters,
   actions
-}
+};
