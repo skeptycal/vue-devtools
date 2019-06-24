2caf2cb4ac36084342d0f7efa8ad65a67d322e7c
skeptycal <skeptycal@gmail.com>
2019-06-24 12:43:48 -0500

    formatting

--- shells/dev/target/TransitionExample.vue
+++ shells/dev/target/TransitionExample.vue
@@ -13,7 +13,12 @@
       <button v-on:click="++count">Add</button>
       <button v-on:click="--count">Remove</button>
       <transition-group name="list" tag="p">
-        <component :is="'TestComponent'" v-for="item in count" v-bind:key="item" class="list-item">
+        <component
+          :is="'TestComponent'"
+          v-for="item in count"
+          v-bind:key="item"
+          class="list-item"
+        >
           {{ item }}
         </component>
       </transition-group>
@@ -27,21 +32,22 @@ export default {
     return {
       show: true,
       count: 5
-    }
+    };
   },
   components: {
     TestComponent: {
       render(h) {
-        return h('div', {}, this.$slots.default)
+        return h("div", {}, this.$slots.default);
       }
     }
   }
-}
+};
 </script>
 
 <style scoped>
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
