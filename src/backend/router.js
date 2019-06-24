1b3191d796c35f570cda8c6c437a26e5191827c5
skeptycal <skeptycal@gmail.com>
2019-06-24 12:44:28 -0500

    formatting

--- src/backend/router.js
+++ src/backend/router.js
@@ -1,66 +1,72 @@
-import { stringify } from '../util'
+import { stringify } from "../util";
 
-export function initRouterBackend (Vue, bridge, rootInstances) {
-  let recording = true
+export function initRouterBackend(Vue, bridge, rootInstances) {
+  let recording = true;
 
   const getSnapshot = () => {
-    const routeChanges = []
+    const routeChanges = [];
     rootInstances.forEach(instance => {
-      const router = instance._router
+      const router = instance._router;
       if (router && router.options && router.options.routes) {
-        routeChanges.push(...router.options.routes)
+        routeChanges.push(...router.options.routes);
       }
-    })
+    });
     return stringify({
       routeChanges
-    })
-  }
+    });
+  };
 
-  bridge.send('routes:init', getSnapshot())
+  bridge.send("routes:init", getSnapshot());
 
-  bridge.on('router:toggle-recording', enabled => {
-    recording = enabled
-  })
+  bridge.on("router:toggle-recording", enabled => {
+    recording = enabled;
+  });
 
   rootInstances.forEach(instance => {
-    const router = instance._router
+    const router = instance._router;
 
     if (router) {
       router.afterEach((to, from) => {
-        if (!recording) return
-        bridge.send('router:changed', stringify({
-          to,
-          from,
-          timestamp: Date.now()
-        }))
-      })
-      bridge.send('router:init', stringify({
-        mode: router.mode,
-        current: {
-          from: router.history.current,
-          to: router.history.current,
-          timestamp: Date.now()
-        }
-      }))
+        if (!recording) return;
+        bridge.send(
+          "router:changed",
+          stringify({
+            to,
+            from,
+            timestamp: Date.now()
+          })
+        );
+      });
+      bridge.send(
+        "router:init",
+        stringify({
+          mode: router.mode,
+          current: {
+            from: router.history.current,
+            to: router.history.current,
+            timestamp: Date.now()
+          }
+        })
+      );
 
       if (router.matcher && router.matcher.addRoutes) {
-        const addRoutes = router.matcher.addRoutes
-        router.matcher.addRoutes = function (routes) {
-          routes.forEach((item) => {
-            bridge.send('routes:changed', stringify(item))
-          })
-          addRoutes.call(this, routes)
-        }
+        const addRoutes = router.matcher.addRoutes;
+        router.matcher.addRoutes = function(routes) {
+          routes.forEach(item => {
+            bridge.send("routes:changed", stringify(item));
+          });
+          addRoutes.call(this, routes);
+        };
       }
     }
-  })
+  });
 }
 
-export function getCustomRouterDetails (router) {
+export function getCustomRouterDetails(router) {
   return {
     _custom: {
-      type: 'router',
-      display: 'VueRouter',
+      type: "router",
+      display: "VueRouter",
       value: {
         options: router.options,
         currentRoute: router.currentRoute
@@ -69,5 +75,5 @@ export function getCustomRouterDetails (router) {
         abstract: true
       }
     }
-  }
+  };
 }
