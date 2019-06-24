6f019219e886f70661cf07614104df06411324ad
skeptycal <skeptycal@gmail.com>
2019-06-24 12:43:39 -0500

    formatting

--- shells/dev/target/EventChildCond.vue
+++ shells/dev/target/EventChildCond.vue
@@ -1,27 +1,29 @@
 <template>
   <div>
-    <button class="btn-emit-event-cond" @click="emitLogEvent">Emit from cond</button>
+    <button class="btn-emit-event-cond" @click="emitLogEvent">
+      Emit from cond
+    </button>
   </div>
 </template>
 
 <script>
 export default {
   methods: {
-    emitLogEvent () {
+    emitLogEvent() {
       const data = {
-        componentName: 'EventChild1',
-        string: 'Lorem ipsum',
+        componentName: "EventChild1",
+        string: "Lorem ipsum",
         complex: {
-          string: 'Lorem ipsum',
+          string: "Lorem ipsum",
           object: {
             number: 23,
             boolean: true,
             array: [1, 2, 3, 4, 5]
           }
         }
-      }
-      this.$emit('log', data)
+      };
+      this.$emit("log", data);
     }
   }
-}
+};
 </script>
