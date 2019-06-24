8468f155eb99cc0c79e11001393fa2b0472b285a
skeptycal <skeptycal@gmail.com>
2019-06-24 12:45:04 -0500

    formatting

--- src/devtools/views/components/ComponentInspector.vue
+++ src/devtools/views/components/ComponentInspector.vue
@@ -1,9 +1,6 @@
 <template>
   <scroll-pane>
-    <action-header
-      v-show="hasTarget"
-      slot="header"
-    >
+    <action-header v-show="hasTarget" slot="header">
       <span class="title">
         <span class="title-bracket">&lt;</span>
         <span>{{ targetName }}</span>
@@ -11,15 +8,9 @@
       </span>
       <div class="search">
         <VueIcon icon="search" />
-        <input
-          v-model.trim="filter"
-          placeholder="Filter inspected data"
-        >
+        <input v-model.trim="filter" placeholder="Filter inspected data" />
       </div>
-      <VueLoadingIndicator
-        v-if="loading"
-        class="primary"
-      />
+      <VueLoadingIndicator v-if="loading" class="primary" />
       <a
         v-if="$isChrome"
         v-tooltip="'Inspect DOM'"
@@ -31,7 +22,9 @@
       </a>
       <a
         v-if="fileIsPath"
-        v-tooltip="$t('ComponentInspector.openInEditor.tooltip', { file: target.file })"
+        v-tooltip="
+          $t('ComponentInspector.openInEditor.tooltip', { file: target.file })
+        "
         class="button"
         @click="openInEditor"
       >
@@ -40,22 +33,13 @@
       </a>
     </action-header>
     <template slot="scroll">
-      <section
-        v-if="!hasTarget"
-        class="notice"
-      >
+      <section v-if="!hasTarget" class="notice">
         <div>Select a component instance to inspect.</div>
       </section>
-      <div
-        v-else-if="!target.state || !target.state.length"
-        class="notice"
-      >
+      <div v-else-if="!target.state || !target.state.length" class="notice">
         <div>This instance has no reactive state.</div>
       </div>
-      <section
-        v-else
-        class="data"
-      >
+      <section v-else class="data">
         <state-inspector
           :state="filteredState"
           class="component-state-inspector"
@@ -66,11 +50,16 @@
 </template>
 
 <script>
-import ScrollPane from 'components/ScrollPane.vue'
-import ActionHeader from 'components/ActionHeader.vue'
-import StateInspector from 'components/StateInspector.vue'
-import { searchDeepInObject, sortByKey, classify, openInEditor } from 'src/util'
-import groupBy from 'lodash.groupby'
+import ScrollPane from "components/ScrollPane.vue";
+import ActionHeader from "components/ActionHeader.vue";
+import StateInspector from "components/StateInspector.vue";
+import {
+  searchDeepInObject,
+  sortByKey,
+  classify,
+  openInEditor
+} from "src/util";
+import groupBy from "lodash.groupby";
 
 export default {
   components: {
@@ -90,59 +79,70 @@ export default {
     }
   },
 
-  data () {
+  data() {
     return {
-      filter: ''
-    }
+      filter: ""
+    };
   },
 
   computed: {
-    hasTarget () {
-      return this.target.id != null
+    hasTarget() {
+      return this.target.id != null;
     },
 
-    targetName () {
-      return this.$shared.classifyComponents ? classify(this.target.name) : this.target.name
+    targetName() {
+      return this.$shared.classifyComponents
+        ? classify(this.target.name)
+        : this.target.name;
     },
 
-    filteredState () {
-      return groupBy(sortByKey(this.target.state.filter(el => {
-        return searchDeepInObject({
-          [el.key]: el.value
-        }, this.filter)
-      })), 'type')
+    filteredState() {
+      return groupBy(
+        sortByKey(
+          this.target.state.filter(el => {
+            return searchDeepInObject(
+              {
+                [el.key]: el.value
+              },
+              this.filter
+            );
+          })
+        ),
+        "type"
+      );
     },
 
     // Checks if the file is actually a path (e.g. '/path/to/file.vue'), or
     // only the basename of a pre-compiled 3rd-party component (e.g. 'file.vue')
-    fileIsPath () {
-      return this.target.file && /[/\\]/.test(this.target.file)
+    fileIsPath() {
+      return this.target.file && /[/\\]/.test(this.target.file);
     }
   },
 
   methods: {
-    inspectDOM () {
-      if (!this.hasTarget) return
+    inspectDOM() {
+      if (!this.hasTarget) return;
       if (this.$isChrome) {
         chrome.devtools.inspectedWindow.eval(
           `inspect(window.__VUE_DEVTOOLS_INSTANCE_MAP__.get("${this.target.id}").$el)`
-        )
+        );
       } else {
-        window.alert('DOM inspection is not supported in this shell.')
+        window.alert("DOM inspection is not supported in this shell.");
       }
     },
 
-    openInEditor () {
-      const file = this.target.file
-      openInEditor(file)
+    openInEditor() {
+      const file = this.target.file;
+      openInEditor(file);
     }
   }
-}
+};
 </script>
 
 <style lang="stylus" scoped>
-.title
-  white-space nowrap
-  position relative
-  top -1px
+.title {
+  white-space: nowrap;
+  position: relative;
+  top: -1px;
+}
 </style>
